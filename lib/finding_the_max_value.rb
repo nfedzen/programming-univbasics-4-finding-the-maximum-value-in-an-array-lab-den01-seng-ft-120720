f833b533959c0e0877b1ec683c1c2c5541fbc744
Nathan Fedzen <nfedzen@hotmail.com>
2020-11-09 22:44:15 +0000

    Done.

--- lib/finding_the_max_value.rb
+++ lib/finding_the_max_value.rb
@@ -1,3 +1,10 @@
 def find_max_value(array)
+  highest_number = 0 
+  array.each do |number|
+    if highest_number < number
+      highest_number = number
+    end
+  end
+  return highest_number
   # Add your solution here
 end
